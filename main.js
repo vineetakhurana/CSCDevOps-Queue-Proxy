69011f78f8c7d5abd4e827e31f369eeea4ed0db1
vineetak <Vineeta.Khurana@netapp.com>
2015-04-04 15:32:40 -0400

    abc1

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var PORT = args[0];
 // REDIS
 //var client = redis.createClient(6379, '127.0.0.1', {})
 var REDIS_PORT = args[1]
+console.log(REDIS_PORT)
 var client = redis.createClient(REDIS_PORT, '127.0.0.1', {})
 
 var x;
